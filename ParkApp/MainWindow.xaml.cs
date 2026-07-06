b89a1aacdf16421de1ce5e54659dc948eefe8761
Rudzeris <ruzil1atangulov@gmail.com>
2026-07-07 01:19:56 +0500

    # 1 Создал замену текста

--- ParkApp/MainWindow.xaml.cs
+++ ParkApp/MainWindow.xaml.cs
@@ -1,13 +1,48 @@
+using System.IO;
+using System.Windows;
+using Xceed.Words.NET;
+
 namespace ParkApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var car = new Car("Lada", "1234AB");
+
+            var templatePath = @"F:\Atangulov\Projects\ParkApp\Templates\template.docx";
+
+            // Создаём папку Output рядом с exe
+            var outputDir = Path.Combine(Directory.GetCurrentDirectory(), "Output");
+            Directory.CreateDirectory(outputDir);
+
+            var outputPath = Path.Combine(outputDir, $"Car_{car.Number}.docx");
+
+            var doc = DocX.Load(templatePath);
+
+            doc.ReplaceText("{car_model}", car.Model);
+            doc.ReplaceText("{car_number}", car.Number);
+
+            doc.SaveAs(outputPath);
+
+            MessageBox.Show("Документ создан!");
+        }
+    }
+
+    class Car
+    {
+        public string Model { get; private set; }
+        public string Number { get; private set; }
+
+        public Car(string model, string number)
+        {
+            Model = model;
+            Number = number;
         }
     }
 }
